185046d2f3ca986fcc65b142c2e0729a0cb39028
Akmal Sabirov <60604830+blxxdymxve@users.noreply.github.com>
2021-02-24 17:04:21 +0300

    nothing

--- task2/main2.py
+++ task2/main2.py
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import sys
 from PyQt5 import QtWidgets, QtCore
 import design2
@@ -39,45 +38,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-=======
-import sys
-from PyQt5 import QtWidgets, QtCore
-import design2
-
-class ExampleApp(QtWidgets.QMainWindow, design2.Ui_Form):
-    def __init__(self):
-        super().__init__()
-        self.setupUi(self)
-        self.label_2.setAlignment(QtCore.Qt.AlignCenter)
-        self.lineEdit.setAlignment(QtCore.Qt.AlignCenter)
-        self.pushButton.clicked.connect(self.calculator)
-
-    def calculator(self):
-        x = self.lineEdit.text()
-        y = self.lineEdit_2.text()
-
-        x = int(x)
-        y = int(y)
-
-        many = []
-        for s in range(x, y+1):
-            if s % 400 == 0:
-                many.append(str(s))
-            else:
-                if s % 4 == 0:
-                    many.append(str(s))
-                else:
-                    pass
-        many = ", ".join(many)
-
-        self.label_2.setText(many)
-
-def main():
-    app = QtWidgets.QApplication(sys.argv)
-    window = ExampleApp()
-    window.show()
-    app.exec_()
-
-if __name__ == '__main__':
-    main()
->>>>>>> Stashed changes
--- task3/main.py
+++ task3/main.py
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 from random import randint
 
 x = int(input("Введите начало промежутка: "))
@@ -19,26 +18,3 @@ for elem in array:
 print("Исходный массив: ", array)
 print("Положительный массив: ", array_plus)
 print("Отрицательный массив: ", array_minus)
-=======
-from random import randint
-
-x = int(input("Введите начало промежутка: "))
-y = int(input("Введите конец промежутка: "))
-
-array = [randint(x, y) for elem in range(10)]
-array_plus = []
-array_minus = []
-
-for elem in array:
-    if elem > 0:
-        array_plus.append(elem)
-    elif elem < 0:
-        array_minus.append(elem)
-    else:
-        pass
-
-print("Исходный массив: ", array)
-print("Положительный массив: ", array_plus)
-print("Отрицательный массив: ", array_minus)
->>>>>>> Stashed changes
- 
