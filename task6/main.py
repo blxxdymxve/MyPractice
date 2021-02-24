a8251f2c3520e379154a8cc342db66426112f6d3
Akmal Sabirov <60604830+blxxdymxve@users.noreply.github.com>
2021-02-24 10:20:52 +0300

    nothing

--- task6/main.py
+++ task6/main.py
@@ -6,5 +6,10 @@ def random_line(afile):
         if random.randrange(num): continue
         line = aline
     print(line)
+
 random_line(open('text.txt', encoding='utf8'))
 'one\n'
+while True:
+    if input():
+        random_line(open('text.txt', encoding='utf8'))
+        'one\n'
