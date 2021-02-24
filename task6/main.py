df8da260e21dba06eba20793ab6e0b9026c3a262
Akmal Sabirov <60604830+blxxdymxve@users.noreply.github.com>
2021-02-24 10:16:30 +0300

    nothing

---
+++ task6/main.py
@@ -0,0 +1,10 @@
+import random
+
+def random_line(afile):
+    line = next(afile)
+    for num, aline in enumerate(afile, 2):
+        if random.randrange(num): continue
+        line = aline
+    print(line)
+random_line(open('text.txt', encoding='utf8'))
+'one\n'
