3f05a3a14cc3bb747143be552edf835488968bea
Akmal Sabirov <60604830+blxxdymxve@users.noreply.github.com>
2021-02-28 00:32:46 +0300

    nothing new

--- task6_2_1/main.py
+++ task6_2_1/main.py
@@ -33,15 +33,3 @@ with open('coordinates.txt', 'r', encoding='utf8') as f:
     ABC = Hexagon(a, b, c, d)
 
 print(ABC.perimeter)
-
-# a = Point(0, 0)
-#
-# b = Point(0, 1)
-#
-# c = Point(2, 1)
-#
-# d = Point(2, 0)
-#
-# ABC = Hexagon(a, b, c, d)
-#
-# print(ABC.perimeter)
